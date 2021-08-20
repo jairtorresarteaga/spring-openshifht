bc304ac8eba6efdce8fff261bb72f5b71e1ddd6a
jairtorresarteaga <jairtorresarteaga@gmail.com>
2021-08-19 21:42:12 -0500

    Add files via upload

---
+++ src/test/java/com/example/spring/openshifht/com/SpringOpenshifhtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.openshifht.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenshifhtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
